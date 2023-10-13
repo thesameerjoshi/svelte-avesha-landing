275f9c68bea18a755f827d0a5b8d7c0766887ff6
thesameerjoshi <sameer.joshi@smaitic.com>
2023-10-13 10:01:30 +0545

    Husky Precommit Changed

--- src/pages/home/constant.js
+++ src/pages/home/constant.js
@@ -4,72 +4,65 @@ import SamartScaler from '../../assets/SmartScaler.svg';
 import STD from '../../assets/STD.svg';
 
 const cards = [
-    {
-        title: 'Kubeslice',
-        image: KubeSlice,
-        description:
-            'This section provides a high-level introduction and understanding of Kubeslice,',
-        link: 'https://docs.kubeslice.io',
-        points: [
-            { point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
-            { point: 'High level architecture', link: 'https://docs.kubeslice.io' },
-            { point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
-            { point: 'Key features', link: 'https://docs.kubeslice.io' }
-        ],
-        variant: 'orange'
-    },
-    {
-        title: 'KubeSlice Manager',
-        image: KubeSliceManager,
-        description:
-            'This section provides a high-level introduction and understanding of Kubeslice,',
-        link: 'https://docs.kubeslice.io',
-        points: [
-            { point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
-            { point: 'High level architecture', link: 'https://docs.kubeslice.io' },
-            { point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
-            { point: 'Key features', link: 'https://docs.kubeslice.io' }
-        ],
-        variant: 'orange'
-    },
-    {
-        title: 'Smart Scaler',
-        image: SamartScaler,
-        description:
-            'This section provides a high-level introduction and understanding of Kubeslice,',
-        link: 'https://docs.kubeslice.io',
-        points: [
-            { point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
-            { point: 'High level architecture', link: 'https://docs.kubeslice.io' },
-            { point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
-            { point: 'Key features', link: 'https://docs.kubeslice.io' }
-        ],
-        variant: 'green'
-    },
-    {
-        title: 'Smart Traffic Director',
-        image: STD,
-        description:
-            'This section provides a high-level introduction and understanding of Kubeslice,',
-        link: 'https://docs.kubeslice.io',
-        points: [
-            { point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
-            { point: 'High level architecture', link: 'https://docs.kubeslice.io' },
-            { point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
-            { point: 'Key features', link: 'https://docs.kubeslice.io' }
-        ],
-        variant: 'green'
-    }
+	{
+		title: 'Kubeslice',
+		image: KubeSlice,
+		description: 'This section provides a high-level introduction and understanding of Kubeslice,',
+		link: 'https://docs.kubeslice.io',
+		points: [
+			{ point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
+			{ point: 'High level architecture', link: 'https://docs.kubeslice.io' },
+			{ point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
+			{ point: 'Key features', link: 'https://docs.kubeslice.io' }
+		],
+		variant: 'orange'
+	},
+	{
+		title: 'KubeSlice Manager',
+		image: KubeSliceManager,
+		description: 'This section provides a high-level introduction and understanding of Kubeslice,',
+		link: 'https://docs.kubeslice.io',
+		points: [
+			{ point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
+			{ point: 'High level architecture', link: 'https://docs.kubeslice.io' },
+			{ point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
+			{ point: 'Key features', link: 'https://docs.kubeslice.io' }
+		],
+		variant: 'orange'
+	},
+	{
+		title: 'Smart Scaler',
+		image: SamartScaler,
+		description: 'This section provides a high-level introduction and understanding of Kubeslice,',
+		link: 'https://docs.kubeslice.io',
+		points: [
+			{ point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
+			{ point: 'High level architecture', link: 'https://docs.kubeslice.io' },
+			{ point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
+			{ point: 'Key features', link: 'https://docs.kubeslice.io' }
+		],
+		variant: 'green'
+	},
+	{
+		title: 'Smart Traffic Director',
+		image: STD,
+		description: 'This section provides a high-level introduction and understanding of Kubeslice,',
+		link: 'https://docs.kubeslice.io',
+		points: [
+			{ point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
+			{ point: 'High level architecture', link: 'https://docs.kubeslice.io' },
+			{ point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
+			{ point: 'Key features', link: 'https://docs.kubeslice.io' }
+		],
+		variant: 'green'
+	}
 ];
 
 const opensourcePoints = [
-    { point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
-    { point: 'High level architecture', link: 'https://docs.kubeslice.io' },
-    { point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
-    { point: 'Key features', link: 'https://docs.kubeslice.io' }
+	{ point: 'What is kubeslice', link: 'https://docs.kubeslice.io' },
+	{ point: 'High level architecture', link: 'https://docs.kubeslice.io' },
+	{ point: 'Kubeslice components', link: 'https://docs.kubeslice.io' },
+	{ point: 'Key features', link: 'https://docs.kubeslice.io' }
 ];
 
-export {
-    cards,
-    opensourcePoints as points
-}
+export { cards, opensourcePoints as points };
